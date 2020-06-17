d9e957c76667970dab3c235d3736115096dad77b
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-06-17 18:13:50 -0400

    some changes

--- src/pynwb/ndx_hierarchical_behavioral_data/transcription_io.py
+++ src/pynwb/ndx_hierarchical_behavioral_data/transcription_io.py
@@ -1,4 +1,3 @@
-
 import os
 import glob
 from ndx_hierarchical_behavioral_data.definitions.transcription import phonemes, syllables, words, sentences
@@ -14,50 +13,47 @@ def read_transcription_data(path_to_files, filename_pattern):
     return lngg_level
 
 
-
-path_to_files = 'C:/Users/Admin/Desktop/Ben Dichter/Chang Lab/convert/TimitSounds (Transcriptions)/TimitSounds (Transcriptions)/fadg'
+dpath = 'C:/Users/Admin/Desktop/Ben Dichter/Chang Lab/convert/TimitSounds (Transcriptions)/TimitSounds (' \
+              'Transcriptions)/fadg '
 
 # Read data
-phonemes_data = read_transcription_data(path_to_files, '*phn')
-syllables_data = read_transcription_data(path_to_files, '*syll')
-words_data = read_transcription_data(path_to_files, '*wrd')
-sentences_data = read_transcription_data(path_to_files, '*[0-9].txt')
+phonemes_data = read_transcription_data(dpath, '*phn')
+syllables_data = read_transcription_data(dpath, '*syll')
+words_data = read_transcription_data(dpath, '*wrd')
+sentences_data = read_transcription_data(dpath, '*[0-9].txt')
 
 # Join words
 for i in range(len(sentences_data)):
     sentences_data[i] = sentences_data[i][0:2] + [' '.join(sentences_data[i][2:])]
 
-
 # phonemes
 for phonemes_sample in phonemes_data:
-    phonemes.add_interval(label=phonemes_sample[2], start_time=float(phonemes_sample[0]), stop_time=float(phonemes_sample[1]))
+    phonemes.add_interval(label=phonemes_sample[2], start_time=float(phonemes_sample[0]),
+                          stop_time=float(phonemes_sample[1]))
 
 # syllables
-## To figure out how to create syllables_keys automatically?
-syllables_keys = {'0': 'h#', '1': 'bricks', '2': 'are', '3': 'an', '4': 'al', 
+# To figure out how to create syllables_keys automatically?
+syllables_keys = {'0': 'h#', '1': 'bricks', '2': 'are', '3': 'an', '4': 'al',
                   '5': 'ter', '6': 'na', '7': 'tive'}
 
-syllables.add_column('phonemes', '')
-syllables.add_column('word_onset', '')
-syllables.add_column('syllable_number', '')
-syllables.add_column('speech_sound', '')
-
+syllables.add_column('word_onset', 'word onset')
+syllables.add_column('speech_sound', '-1 for consonants, 0 for unstressed vowel, 1 - primary stress, 2 - secondary '
+                                     'stress')
 
 for j, syllables_sample in enumerate(syllables_data):
     syllables.add_interval(label=syllables_keys[syllables_sample[4]],
                            start_time=float(syllables_sample[0]),
                            stop_time=float(syllables_sample[1]),
-                           phonemes=syllables_sample[2],
                            word_onset=syllables_sample[3],
-                           syllable_number=syllables_sample[4],
                            speech_sound=syllables_sample[5],
                            next_tier=[j])
 
-## To figure out how to assign list of indices to next_tier automatically?
+# To figure out how to assign list of indices to next_tier automatically?
 words_keys = {'bricks': [1], 'are': [2], 'an': [3], 'alternative': [4, 5, 6, 7]}
 for words_sample in words_data:
-    words.add_interval(start_time=float(words_sample[0]), stop_time=float(words_sample[1]), label=words_sample[2], next_tier=words_keys[words_sample[2]])
-
+    words.add_interval(start_time=float(words_sample[0]), stop_time=float(words_sample[1]), label=words_sample[2],
+                       next_tier=words_keys[words_sample[2]])
 
 for sentences_sample in sentences_data:
-    sentences.add_interval(start_time=float(sentences_sample[0]), stop_time=float(sentences_sample[1]), label=sentences_sample[2], next_tier=list(range(len(words_data))))
+    sentences.add_interval(start_time=float(sentences_sample[0]), stop_time=float(sentences_sample[1]),
+                           label=sentences_sample[2], next_tier=list(range(len(words_data))))
