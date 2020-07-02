5fec814ee92fb44ea62a13e4a7d8cc9499438618
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-07-01 22:25:14 -0400

    docstring

--- src/pynwb/ndx_hierarchical_behavioral_data/transcription_io.py
+++ src/pynwb/ndx_hierarchical_behavioral_data/transcription_io.py
@@ -8,6 +8,27 @@ from ndx_hierarchical_behavioral_data.definitions.transcription import phonemes,
 
 
 def timitsounds_reader(path_to_files, filename_pattern, add_headings, separator=' '):
+    """Read a file from specific path and convert it to a DataFrame
+
+        For a given path, and specific file name/pattern, this function reads the file
+        and stores the data in a pandas DataFrame. Name of columns should be provided in add_headings.
+
+        Parameters
+        ----------
+        path_to_files: str
+            Path to the files
+        filename_pattern: str
+            name or specific pattern in the file name
+        add_headings: list
+            list of columns' headers
+        separator: str
+            separator
+
+        Returns
+        ----------
+        pandas.DataFrame
+
+        """
     fpath0 = os.path.join(path_to_files, filename_pattern)
     f_lngg_level = glob.glob(fpath0)[0]
     lngg_level = pd.read_csv(f_lngg_level,
@@ -17,6 +38,25 @@ def timitsounds_reader(path_to_files, filename_pattern, add_headings, separator=
 
 
 def intensity_reader(path_to_files, filename_pattern='*Intensity', add_headings=['h1']):
+    """Read intensity data and convert them to a DataFrame
+
+        For a given path, and specific file name/pattern, this function reads the file of intensity data
+        and stores the data in a pandas DataFrame. Name of columns can be provided in add_headings.
+
+        Parameters
+        ----------
+        path_to_files: str
+            Path to the files
+        filename_pattern: str
+            name or specific pattern in the file name
+        add_headings: list
+            list of columns' headers
+
+        Returns
+        ----------
+        pandas.DataFrame
+
+        """
     fpath0 = os.path.join(path_to_files, filename_pattern)
     f_lngg_level = glob.glob(fpath0)[0]
     with open(f_lngg_level, 'r') as f:
@@ -33,6 +73,21 @@ def intensity_reader(path_to_files, filename_pattern='*Intensity', add_headings=
 
 
 def syllables_data_extractor(syllables_phonemes_data):
+    """Applies some preprocessing steps on provided format of syllables data to extract syllable data
+
+        For given format of syllables data, some preprocessing steps are required to extract syllables and put them
+        in a DataFrame.
+
+        Parameters
+        ----------
+        syllables_phonemes_data: pandas.DataFrame
+            provided format of syllables data stored in a DataFrame
+
+        Returns
+        ----------
+        pandas.DataFrame
+
+        """
     uniq_syl = syllables_phonemes_data['syllable_number'].unique()
     syllables_data = pd.DataFrame(columns=['start_time', 'stop_time', 'label', 'syllable_number'])
     for i in uniq_syl:
@@ -50,6 +105,21 @@ def syllables_data_extractor(syllables_phonemes_data):
 
 
 def words_syllables_ind(syllables_phonemes_data):
+    """Extracts related information for the next_tier in the word table
+
+        Provided format of the syllables data contains word information. This function extract this information for
+        use in building word table and its relation to syllables.
+
+        Parameters
+        ----------
+        syllables_phonemes_data: pandas.DataFrame
+            provided format of syllables data stored in a DataFrame
+
+        Returns
+        ----------
+        list
+
+        """
     word_onset = [i for i, val in enumerate(list(syllables_phonemes_data['word_onset'])) if val == 2] + [
         len(syllables_phonemes_data['word_onset']) - 1]
     syllables_onset = [i for i, val in enumerate(list(syllables_phonemes_data['word_onset'])) if val == 1]
@@ -69,6 +139,34 @@ def words_syllables_ind(syllables_phonemes_data):
 
 
 def timitsounds_df(path_to_files):
+    """Given path to the files, this function reads the files and stores the data in appropriate form in DataFrame
+
+        For given path to the files, this function reads the files, extracts related phonemes, syllables, words, and
+        sentences data and also pitch, intensity, and formant data and stores them in DataFrames.
+
+        Parameters
+        ----------
+        path_to_files: str
+            Path to the files
+
+        Returns
+        ----------
+        phonemes_data
+            pandas.DataFrame
+        syllables_data
+            pandas.DataFrame
+        words_data
+            pandas.DataFrame
+        sentences_data
+            pandas.DataFrame
+        pitch_data
+            pandas.DataFrame
+        formant_data
+            pandas.DataFrame
+        intensity_data
+            pandas.DataFrame
+
+        """
     # Create phonemes dataframe
     phonemes_data = timitsounds_reader(path_to_files, '*phn', ['start_time', 'stop_time', 'label'])
 
@@ -101,6 +199,47 @@ def timitsounds_df(path_to_files):
 
 def timitsounds_converter(phonemes_data, syllables_data, words_data, sentences_data, pitch_data, formant_data,
                           intensity_data):
+    """Converts phonemes, syllables, words, sentences, pitch, formant, and intensity data into
+        hierarchical table format
+
+        For given phonemes, syllables, words, sentences, pitch, formant, and intensity data, this
+        function converts the data into hierarchical table format.
+
+        Parameters
+        ----------
+        phonemes_data: pandas.DataFrame
+            phonemes' DataFrame
+        syllables_data: pandas.DataFrame
+            syllable' DataFrame
+        words_data: pandas.DataFrame
+            word' DataFrame
+        sentences_data: pandas.DataFrame
+            sentences' DataFrame
+        pitch_data: pandas.DataFrame
+            pitch DataFrame
+        formant_data: pandas.DataFrame
+            formant DataFrame
+        intensity_data: pandas.DataFrame
+            intensity DataFrame
+
+        Returns
+        ----------
+        phonemes
+            pynwb.epoch.TimeIntervals
+        syllables
+            ndx_hierarchical_behavioral_data.hierarchical_behavioral_data.HierarchicalBehavioralTable
+        words
+            ndx_hierarchical_behavioral_data.hierarchical_behavioral_data.HierarchicalBehavioralTable
+        sentences
+            ndx_hierarchical_behavioral_data.hierarchical_behavioral_data.HierarchicalBehavioralTable
+        pitch_ts
+            pynwb.base.TimeSeries
+        formant_ts
+            pynwb.base.TimeSeries
+        intensity_ts
+            pynwb.base.TimeSeries
+
+        """
     # phonemes
     for ind in phonemes_data.index:
         phonemes.add_interval(label=phonemes_data['label'][ind], start_time=float(phonemes_data['start_time'][ind]),
