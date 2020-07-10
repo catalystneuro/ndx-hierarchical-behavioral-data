fb79491e98b1fef057f67cfc305ae01048937ae4
luiztauffer <luiz_tauffer@hotmail.com>
2020-07-10 11:37:19 +0700

    update testing

--- src/pynwb/tests/test_io.py
+++ src/pynwb/tests/test_io.py
@@ -1,9 +1,7 @@
-import datetime
-
+from ndx_hierarchical_behavioral_data.definitions.transcription import TIPhonemes, HBTSyllables, HBTWords, HBTSentences
 from pynwb import NWBHDF5IO, NWBFile
 from pynwb.testing import TestCase, remove_test_file
-
-from ndx_hierarchical_behavioral_data.definitions.transcription import phonemes, syllables, words, sentences
+import datetime
 
 
 class TestTranscription(TestCase):
@@ -25,21 +23,24 @@ class TestTranscription(TestCase):
         file matches the original sentences table.
         """
 
+        phonemes = TIPhonemes()
         phonemes.add_column('max_pitch', 'maximum pitch for this phoneme. NaN for unvoiced')
 
         for i, p in enumerate('abcdefghijkl'):
             phonemes.add_interval(label=p, start_time=float(i), stop_time=float(i + 1), max_pitch=i ** 2)
 
+        syllables = HBTSyllables(lower_tier_table=phonemes)
         syllables.add_interval(label='abc', next_tier=[0, 1, 2])
         syllables.add_interval(label='def', next_tier=[3, 4, 5])
         syllables.add_interval(label='ghi', next_tier=[6, 7, 8])
         syllables.add_interval(label='jkl', next_tier=[9, 10, 11])
 
+        words = HBTWords(lower_tier_table=syllables)
         words.add_column('emphasis', 'boolean indicating whether this word was emphasized')
-
         words.add_interval(label='A-F', next_tier=[0, 1], emphasis=False)
         words.add_interval(label='G-L', next_tier=[2, 3], emphasis=True)
 
+        sentences = HBTSentences(lower_tier_table=words)
         sentences.add_interval(label='A-L', next_tier=[0, 1])
 
         mod = self.nwbfile.create_processing_module('test_mod', 'test_mod')
